09cdb136e74d8d15b92b8d0db8dca76cfda40964
KagishoPrince <kagishomangaba.dev@gmail.com>
2025-08-22 22:07:18 +0200

    Fix Employee and Main classes, update SA ID to long, add printDetails loop

--- src/Employee.java
+++ src/Employee.java
@@ -3,7 +3,7 @@
 public class Employee {
 
     String name;
-    int id;
+    long id;             // changed to long for SA ID
     int age;
     String department;
     String position;
@@ -11,9 +11,8 @@ public class Employee {
     int leaveDays;
     String email;
 
-
-    Employee(String name , int id , int age , String department , double baseSalary , int leaveDays , String email  ) {
-
+    Employee(String name, long id, int age, String department, double baseSalary,
+             int leaveDays, String email) {
         this.name = name;
         this.id = id;
         this.age = age;
@@ -21,12 +20,21 @@ public class Employee {
         this.baseSalary = baseSalary;
         this.leaveDays = leaveDays;
         this.email = email;
-        this.position = "Employee";
+        this.position = "Employee"; // default, can be overridden in subclass
     }
 
     void getSalary() {
-        System.out.println(this.name + "'s BaseSalary is " );
+        System.out.println(this.name + "'s Base Salary is R" + this.baseSalary);
     }
 
-
+    void printDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("Age: " + age);
+        System.out.println("Department: " + department);
+        System.out.println("Position: " + position);
+        System.out.println("Salary: R" + baseSalary);
+        System.out.println("Leave Days: " + leaveDays);
+        System.out.println("Email: " + email);
+    }
 }
