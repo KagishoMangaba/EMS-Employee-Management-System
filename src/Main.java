e99b6c23cc13a03eec854b924f91a687a52c9300
KagishoPrince <kagishomangaba.dev@gmail.com>
2025-08-24 21:33:01 +0200

    Added specific roles to seniors

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,17 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
-        Employee[] employees = new Employee[5];
+        ArrayList<Employee> employees = new ArrayList<>();
 
-        employees[0] = new Employee("Thabo", 9001015234087L, 45, "Finance", 45000.00, 20, "thabo.m@company.co.za");
-        employees[1] = new Employee("Lerato", 2030512340L, 23, "IT", 18000.00, 15, "lerato.d@company.co.za");
-        employees[2] = new Employee("Sipho", 9102075234090L, 32, "IT", 35000.00, 18, "sipho.k@company.co.za");
-        employees[3] = new Employee("Nandi", 8809035234012L, 38, "HR", 42000.00, 20, "nandi.m@company.co.za");
-        employees[4] = new Employee("Kabelo", 604125234075L, 21, "Marketing", 8000.00, 10, "kabelo.t@company.co.za");
+        Employee thabo = new Employee("Thabo", 9001015234087L, 45, "Finance", 45000.00, 20, "thabo.m@company.co.za");
+        Employee lerato = new Employee("Lerato", 2030512340L, 23, "IT", 18000.00, 15, "lerato.d@company.co.za");
+        Employee sipho = new Employee("Sipho", 9102075234090L, 32, "IT", 35000.00, 18, "sipho.k@company.co.za");
 
-        for (Employee e : employees) {
-            e.printDetails();
-            e.getSalary();
-            System.out.println("-------------------");
 
-        }
+
+
 
 
     }
--- src/SeniorDeveloper.java
+++ src/SeniorDeveloper.java
@@ -1,2 +1,17 @@
-public class SeniorDeveloper  {
+public class SeniorDeveloper extends Employee {
+
+    private String BonusMultiplier;
+
+
+    public SeniorDeveloper(String name, long id, int age, String department,
+                           double baseSalary, int leaveDays, String email, String bonusMultiplier) {
+
+        super(name, id, age, department, baseSalary, leaveDays, email);
+
+        this.BonusMultiplier = bonusMultiplier;
+    }
+
+
+
+
 }
