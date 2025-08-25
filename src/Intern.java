cca018d9034546b3833e62d593b8765aa25f6ef6
KagishoPrince <kagishomangaba.dev@gmail.com>
2025-08-25 22:28:09 +0200

    Added new varibles for my classes

--- src/Intern.java
+++ src/Intern.java
@@ -1,11 +1,17 @@
 public class Intern extends Employee{
 
-    String University;
+    private String University;
+    private String DegreeProgram;
+    private String internshipDurationMonths;
+    private double performanceScore;
+
+
 
     Intern(String name, long id, int age, String department, double baseSalary,
            int leaveDays, String email , String University) {
 
         super(name, id, age, department, baseSalary, leaveDays, email);
+
 
         this.University = University;
 
