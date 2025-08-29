8a75ee4a1c4089a220c161678005b4353c0a739b
KagishoPrince <kagishomangaba.dev@gmail.com>
2025-08-29 21:12:51 +0200

    storing all employees in a seperate class for readability

--- src/EmployeeManager.java
+++ src/EmployeeManager.java
@@ -1,14 +1,60 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class EmployeeManager {
+    private List<Employee> employees;
 
-    private ArrayList<Employee> employees = new ArrayList<>();
+    public EmployeeManager() {
+        employees = new ArrayList<>();
+        addAllInterns(); // add all interns on creation
+    }
 
+    // Add any employee
     public void addEmployee(Employee e) {
         employees.add(e);
     }
 
+    // Add 10 interns with all 12 arguments
+    private void addAllInterns() {
+        employees.add(new Intern("Kagisho Prince Mangaba", 9910110001L, 20, "IT", 5000, 5,
+                "kagisho@email.com", "Wits University", "Computer Science", "6", 85, 1));
 
+        employees.add(new Intern("Thabo Mokoena", 9802150002L, 21, "HR", 4800, 4,
+                "thabo@email.com", "UP", "Psychology", "12", 75, 2));
 
+        employees.add(new Intern("Siphiwe Dlamini", 9903230003L, 22, "Marketing", 4500, 6,
+                "siphiwe@email.com", "UCT", "Business Management", "3", 90, 3));
 
+        employees.add(new Intern("Naledi Khumalo", 9505010004L, 23, "Finance", 5200, 5,
+                "naledi@email.com", "Wits University", "Accounting", "6", 60, 1));
+
+        employees.add(new Intern("Lerato Motsepe", 9707070005L, 22, "IT", 5100, 5,
+                "lerato@email.com", "UP", "Software Engineering", "6", 95, 3));
+
+        employees.add(new Intern("Siphesihle Ndlovu", 9909120006L, 21, "HR", 4700, 4,
+                "siphesihle@email.com", "UCT", "Psychology", "12", 70, 2));
+
+        employees.add(new Intern("Mpho Mthethwa", 9808040007L, 22, "Marketing", 4600, 6,
+                "mpho@email.com", "Wits University", "Business", "3", 88, 2));
+
+        employees.add(new Intern("Zinhle Khanyile", 9901010003L, 23, "Finance", 5300, 5,
+                "zinhle@email.com", "UP", "Accounting", "6", 82, 2));
+
+        employees.add(new Intern("Thulani Zuma", 9606100002L, 22, "IT", 5000, 5,
+                "thulani@email.com", "UCT", "Software Engineering", "6", 77, 1));
+
+        employees.add(new Intern("Amanda Molefe", 9703340004L, 21, "HR", 4800, 4,
+                "amanda@email.com", "Wits University", "Psychology", "12", 91, 3));
+    }
+
+    // Display all employees
+    public void displayAllEmployees() {
+        for (Employee e : employees) {
+            e.printDetails();
+            if (e instanceof Intern) {
+                ((Intern) e).evaluatePerformance(); // evaluate interns
+            }
+            System.out.println("----------------------");
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Create an array to store 10 interns
-        Intern[] interns = new Intern[10];
 
-        interns[0] = new Intern("Kagisho Prince Mangaba", 9910110001L, 20, "IT", 5000, 5,
-                "kagisho@email.com", "Wits University", "Computer Science", "6", 85, 1);
 
-        interns[1] = new Intern("Thabo Mokoena", 9802150002L, 21, "HR", 4800, 4,
-                "thabo@email.com", "UP", "Psychology", "12", 75, 2);
-
-        interns[2] = new Intern("Siphiwe Dlamini", 9903230003L, 22, "Marketing", 4500, 6,
-                "siphiwe@email.com", "UCT", "Business Management", "3", 90, 3);
-
-        interns[3] = new Intern("Naledi Khumalo", 0005010004L, 23, "Finance", 5200, 5,
-                "naledi@email.com", "Wits University", "Accounting", "6", 60, 1);
-
-        interns[4] = new Intern("Lerato Motsepe", 0107070005L, 22, "IT", 5100, 5,
-                "lerato@email.com", "UP", "Software Engineering", "6", 95, 3);
-
-        interns[5] = new Intern("Siphesihle Ndlovu", 9909120006L, 21, "HR", 4700, 4,
-                "siphesihle@email.com", "UCT", "Psychology", "12", 70, 2);
-
-        interns[6] = new Intern("Mpho Mthethwa", 9808040007L, 22, "Marketing", 4600, 6,
-                "mpho@email.com", "Wits University", "Business", "3", 88, 2);
-
-        interns[7] = new Intern("Zinhle Khanyile", 0101010006L, 23, "Finance", 5300, 5,
-                "zinhle@email.com", "UP", "Accounting", "6", 82, 2);
-
-        interns[8] = new Intern("Thulani Zuma", 0006100005L, 22, "IT", 5000, 5,
-                "thulani@email.com", "UCT", "Software Engineering", "6", 77, 1);
-
-        interns[9] = new Intern("Amanda Molefe", 010003232442L, 21, "HR", 4800, 4,
-                "amanda@email.com", "Wits University", "Psychology", "12", 91, 3);
-
-        // Loop through interns to print details and evaluate performance
-        for (Intern intern : interns) {
-            intern.printDetails();
-            intern.evaluatePerformance();
-            System.out.println("-----");
-        }
     }
 }
