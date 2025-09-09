395c40196bab5fe3d6faa1c6cbd901cef8a479cc
KagishoPrince <kagishomangaba.dev@gmail.com>
2025-09-09 22:28:20 +0200

    added project management and Mentee Management for seniors

--- src/PayrollService.java
+++ src/PayrollService.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class PayrollService {
     public void printPayroll(List<Employee> employees) {
         for (Employee e : employees) {
-            System.out.println("------------------------------");
+            System.out.println("------");
             e.printDetails();
         }
     }
--- src/SeniorDeveloper.java
+++ src/SeniorDeveloper.java
@@ -1,29 +1,49 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SeniorDeveloper extends Employee {
 
     private String[] programmingLanguages;
-    private int YearsOfExperience;
+    private int yearsOfExperience;
     private List<String> projectsAssigned;
     private List<Employee> mentees;
 
+    // Constructor
     public SeniorDeveloper(String name, long id, int age, String department, double baseSalary,
-                           int leaveDays, String email, String[] programmingLanguages, int YearsOfExperience, String[] initilaProjects) {
+                           int leaveDays, String email, String[] programmingLanguages, int yearsOfExperience, String[] initialProjects) {
 
         super(name, id, age, department, baseSalary, leaveDays, email);
 
         this.programmingLanguages = programmingLanguages;
-        this.YearsOfExperience = YearsOfExperience;
+        this.yearsOfExperience = yearsOfExperience;
         this.projectsAssigned = new ArrayList<>();
-        if (initilaProjects != null) {
-            for (String project : initilaProjects) {
+        if (initialProjects != null) {
+            for (String project : initialProjects) {
                 this.projectsAssigned.add(project);
             }
         }
 
+        this.mentees = new ArrayList<>();
+    }
 
+    // Getters and Setters
+    public String[] getProgrammingLanguages() {
+        return programmingLanguages;
+    }
 
+    public void setProgrammingLanguages(String[] programmingLanguages) {
+        this.programmingLanguages = programmingLanguages;
+    }
+
+    public int getYearsOfExperience() {
+        return yearsOfExperience;
+    }
+
+    public void setYearsOfExperience(int yearsOfExperience) {
+        this.yearsOfExperience = yearsOfExperience;
+    }
 
 
 
